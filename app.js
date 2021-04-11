febb9b17353337437db5a6be35119b3f0803206e
fitripuspitasari <71800875+fitripuspitasari@users.noreply.github.com>
2021-04-11 09:50:33 +0700

    Update app.js
    ya

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama : Fitri Puspitasari | Nim : 51018010 | Jurusan : Sistem Informasi !\n')
 })
 
 app.listen(PORT, HOST)
